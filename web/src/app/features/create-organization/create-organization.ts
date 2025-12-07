53ab2678a49bd888f33eb3eaa6b8ed1da47a76e5
iamcore113 <iamcore113@gmail.com>
2025-12-08 00:37:43 +0800

    web: fix create organization

--- web/src/app/features/create-organization/create-organization.ts
+++ web/src/app/features/create-organization/create-organization.ts
@@ -9,47 +9,50 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { OrganizationService } from '../../core/services/organization/organization.service';
 import { OrganizationMetadata } from '../../core/utils/types';
 
+
+interface OrganizationType {
+  id: string;
+  name: string;
+}
 @Component({
   selector: 'app-create-organization',
-  imports: [MatFormFieldModule, MatSelectModule, MatButtonModule, MatInputModule, MatGridListModule, SimpleCardComponent],
+  imports: [
+    MatFormFieldModule, MatSelectModule,
+    MatButtonModule, MatInputModule,
+    MatGridListModule, SimpleCardComponent,
+    FormsModule, ReactiveFormsModule],
   templateUrl: './create-organization.html',
   styleUrl: './create-organization.css',
 })
 export class CreateOrganization {
   isDisabled = signal<boolean>(false);
   metadata: OrganizationMetadata = {} as OrganizationMetadata;
+  organizationTypes: OrganizationType[] = [] as OrganizationType[];
   private _formBuilder = inject(FormBuilder);
 
   constructor(private org: OrganizationService) {
-    console.log('Fetching organization metadata');
     this.org.getMetadata().subscribe((res) => {
-      console.log('Metadata response: ');
-      console.log(res);
       if ('data' in res) {
         this.metadata = res.data as OrganizationMetadata;
+        this.organizationTypes = this.metadata.organizationTypes;
       }
+
+      console.log(this.metadata);
+      console.log(this.organizationTypes);
     });
   }
 
   organizationForm = this._formBuilder.group({
-    basicInformation: this._formBuilder.group({
-      profileImage: [''],
-      displayName: ['', Validators.required],
-      legalName: ['', Validators.required],
-      organizationType: ['', Validators.required],
-      organizationDescription: [''],
-    }),
-    contactDetails: this._formBuilder.group({
-      countryCode: ['', Validators.required],
-      address: [],
-      country: ['', Validators.required],
-      phoneNo: [],
-      email: ['', [Validators.required, Validators.email]],
-      website: [''],
-      externalLinks: [],
-    }),
-    inviteOtherUsers: this._formBuilder.group({
-      usersToInvite: [''],
-    }),
+    name: ['', Validators.required],
+    email: ['', Validators.required, Validators.email],
+    type: ['', Validators.required],
+    description: [''],
+    country: ['', Validators.required],
+    currency: ['', Validators.required],
+    contactNo: [''],
   });
+
+  onSubmit() {
+    console.log(this.organizationForm.value);
+  }
 }
