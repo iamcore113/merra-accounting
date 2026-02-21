9ed5fa2b3b504644a959a35f238df80151ce8d0b
iamcore113 <iamcore113@gmail.com>
2026-02-21 22:50:05 +0800

    updates

---
+++ web/src/app/shared/api/endpoints/organization.ts
@@ -0,0 +1,5 @@
+import { BASE_URL } from "./base";
+
+const ORGANIZATION_URL = `${BASE_URL}/business/organization/`
+export const GET_ORGANIZATION_USER_BY_ID = `${ORGANIZATION_URL}users/`
+export const CREATE_ORGANIZATION = `${ORGANIZATION_URL}create/`
