5bd3b2eeace523190b2eb49316088329dbd2ed10
neil core <iamcore113@gmail.com>
2026-04-01 21:35:54 +0800

    [ANG] - updates

--- web/src/app/shared/services/auth-service.ts
+++ web/src/app/shared/services/auth-service.ts
@@ -1,9 +1,9 @@
 import { Injectable, inject } from '@angular/core';
-import { CreateAccountRequest } from '../models/auth';
+import { CreateAccountRequest, LoginRequest } from '../models/auth';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Config } from '../models/api_response';
-import { REQUEST_SIGNUP_VERIFICATION_EMAIL_URL, SIGNUP_URL } from '../api/auth';
+import { REQUEST_SIGNUP_VERIFICATION_EMAIL_URL, SIGNIN_URL, SIGNUP_URL } from '../api/auth';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +13,10 @@ export class AuthService {
 
   signup(request: CreateAccountRequest): Observable<Config> {
     return this.http.post<Config>(SIGNUP_URL, request);
+  }
+
+  signin(request: LoginRequest): Observable<Config> {
+    return this.http.post<Config>(SIGNIN_URL, request);
   }
 
   verifyAccount(token: string): Observable<Config> {
--- web/src/app/views/signin/signin.ts
+++ web/src/app/views/signin/signin.ts
@@ -6,8 +6,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {LoginRequest} from '../../shared/models/auth';
+import {LoginRequest, SigninResponse} from '../../shared/models/auth';
+import { AuthService } from '../../shared/services/auth-service';
+import { Config } from '../../shared/models/api_response';
+import { LocalStorageService } from '../../shared/services/local-storage-service';
 @Component({
   selector: 'app-signin',
   imports: [
@@ -17,7 +21,8 @@ import {LoginRequest} from '../../shared/models/auth';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   templateUrl: './signin.html',
   styleUrl: './signin.scss',
@@ -28,7 +33,10 @@ export class Signin implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private localStorage: LocalStorageService,
+    private router: Router
   ) {
     this.signinForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -47,7 +55,27 @@ export class Signin implements OnInit {
 
   onSubmit() {
     if (this.signinForm.valid) {
+      let verifiedData: SigninResponse;
       const req: LoginRequest = this.signinForm.value;
+      this.authService.signin(req).subscribe({
+        next: (response: Config) => {
+          if (response.result && 'data' in response) {
+            verifiedData = (response as any).data as SigninResponse;
+          }
+        },
+        error: (error) => {
+          console.error('Signin failed', error);
+        },
+        complete: () => {
+          this.localStorage.setItem('access_token', verifiedData.tokens.accessToken);
+          this.localStorage.setItem('refresh_token', verifiedData.tokens.refreshToken);
+          const isComplete = verifiedData.accountStatus.isComplete;
+          const getEmail = verifiedData.userdetails.email;
+          if (!isComplete) {
+            this.router.navigate(['/personal-details/', getEmail]);
+          }
+        }
+      });
     }
   }
 }
