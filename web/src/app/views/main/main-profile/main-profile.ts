e91b233b444fa87f0495b47420e52669b14837a2
neil core <iamcore113@gmail.com>
2026-04-18 23:02:37 +0800

    feat: add main profile component with template, styles, and tests

---
+++ web/src/app/views/main/main-profile/main-profile.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-main-profile',
+  imports: [MatIconModule],
+  templateUrl: './main-profile.html',
+  styleUrl: './main-profile.scss',
+})
+export class MainProfile {
+
+}
