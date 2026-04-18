04077b8c94f20e98926d4e86e0327c745aeac784
neil core <iamcore113@gmail.com>
2026-04-18 23:52:11 +0800

    feat: enhance main profile layout with grid structure and animated links

--- web/src/app/views/main/main-profile/main-profile.ts
+++ web/src/app/views/main/main-profile/main-profile.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-main-profile',
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule, RouterOutlet],
   templateUrl: './main-profile.html',
-  styleUrl: './main-profile.scss',
+  styleUrls: ['./main-profile.scss'],
 })
 export class MainProfile {
 
