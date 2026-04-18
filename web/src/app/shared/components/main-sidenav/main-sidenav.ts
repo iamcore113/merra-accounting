4b039bbc5017cb90661bb6651b4ee62e0d738ee6
neil core <iamcore113@gmail.com>
2026-04-18 23:02:24 +0800

    feat: enhance main sidenav with gradient backgrounds and update profile link

--- web/src/app/shared/components/main-sidenav/main-sidenav.ts
+++ web/src/app/shared/components/main-sidenav/main-sidenav.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-main-sidenav',
   standalone: true,
-  imports: [MatListModule, MatIconModule],
+  imports: [MatListModule, MatIconModule, RouterLink],
   templateUrl: './main-sidenav.html',
   styleUrl: './main-sidenav.scss',
 })
