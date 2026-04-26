4d0a9cb374e307bcf3fa46c8e434fec7f263da83
neil core <iamcore113@gmail.com>
2026-04-26 19:53:27 +0800

    fix: correct import statement for GenerationType in UserWorkspaceState class

--- backend/organization/src/main/java/org/merra/entities/UserWorkspaceState.java
+++ backend/organization/src/main/java/org/merra/entities/UserWorkspaceState.java
@@ -6,6 +6,7 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,7 +19,7 @@ import jakarta.persistence.Table;
 @Table(name = "user_workspace_state", schema = "merra_schema")
 public class UserWorkspaceState {
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @OneToOne
