c5a7edf7f1f98f0833ce5c83de257c9b2fc1710a
neil core <iamcore113@gmail.com>
2026-06-19 19:36:15 +0800

    refactor: standardize API versioning, add TypeScript types, and implement country metadata endpoint

--- backend/commons/src/main/java/org/merra/exception/EmailAlreadyEnabledException.java
+++ backend/commons/src/main/java/org/merra/exception/EmailAlreadyEnabledException.java
@@ -2,6 +2,8 @@ package org.merra.exception;
 
 public class EmailAlreadyEnabledException extends RuntimeException {
     public EmailAlreadyEnabledException(String msg) {
+        if (msg == null)
+            msg = "E-mail account is already enabled.";
         super(msg);
     }
 }
